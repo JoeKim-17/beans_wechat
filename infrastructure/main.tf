2cf25d4867b8713c397658a3fff9b9d563bddc04
Tshego-MM <tshegofatso.mohlala@bbd.co.za>
2024-03-06 20:50:13 +0200

    update vpc configurations

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,6 +1,8 @@
 # Create a VPC
 resource "aws_vpc" "beans_wechat_vpc" {
   cidr_block = var.VPC_CIDR
+  enable_dns_support = true
+  enable_dns_hostnames = true
 }
 
 #Internet gateway
@@ -111,7 +113,7 @@ resource "aws_db_instance" "beans-wechat-rds" {
   username = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["username"]
   password = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["password"]
   skip_final_snapshot = true // required to destroy
-  publicly_accessible= false
+  publicly_accessible= true
   identifier = "beans-wechat"
   multi_az = false
   db_subnet_group_name = aws_db_subnet_group.sql_subnet_group.name
