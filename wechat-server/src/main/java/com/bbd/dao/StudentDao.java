483a58aab5bfbe2d9d83aae2d07770a0a9148548
Siya Nkuna <sudosprogrammer@gmail.com>
2024-03-11 14:48:13 +0200

    BWCJLU-27 API endpoints structure setup

---
+++ wechat-server/src/main/java/com/bbd/dao/StudentDao.java
@@ -0,0 +1,17 @@
+package com.bbd.dao;
+
+import com.bbd.models.Student;
+
+import java.util.Collection;
+
+public interface StudentDao {
+    Collection<Student> getAllStudents();
+
+    Student getStudentById(int id);
+
+    void removeStudentById(int id);
+
+    void updateStudent(Student student);
+
+    void insertStudentToDb(Student student);
+}
