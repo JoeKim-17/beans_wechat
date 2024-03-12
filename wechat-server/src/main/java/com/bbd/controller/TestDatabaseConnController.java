b88c0e92395fe28f95fb1ff014c31166d0f879ac
Siya Nkuna <sudosprogrammer@gmail.com>
2024-03-12 14:40:24 +0200

    class name update

--- wechat-server/src/main/java/com/bbd/controller/TestDatabaseConnController.java
+++ wechat-server/src/main/java/com/bbd/controller/TestDatabaseConnController.java
@@ -8,7 +8,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 @RestController
-public class DatabaseController {
+public class TestDatabaseConnectionController {
 
   @Autowired
   private DataSource dataSource;
