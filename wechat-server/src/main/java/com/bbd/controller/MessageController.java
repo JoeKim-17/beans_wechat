fe1947ecd9051baa8fe5c6efbd27e0f0f531903d
Siya Nkuna <sudosprogrammer@gmail.com>
2024-03-12 22:24:52 +0200

    added messages endpoints

---
+++ wechat-server/src/main/java/com/bbd/controller/MessageController.java
@@ -0,0 +1,48 @@
+package com.bbd.controller;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bbd.dao.MessageDao;
+import com.bbd.model.Message;
+
+@RestController
+@RequestMapping("/messages")
+public class MessageController {
+
+  @Autowired
+  private MessageDao messageDao;
+
+  @RequestMapping(method = RequestMethod.GET)
+  public Collection<Message> getAllMessages() {
+    return messageDao.getAllMessages();
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+  public Message getMessageById(@PathVariable("id") int MessageId) {
+    return messageDao.getMessageById(MessageId);
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+  public void deleteMessageById(@PathVariable("id") int MessageId) {
+    messageDao.deleteMessageById(MessageId);
+  }
+
+  @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+  public void updateMessage(@RequestBody Message message) {
+    messageDao.updateMessage(message);
+  }
+
+  @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+  public void insertMessage(@RequestBody Message message) {
+    messageDao.insertMessageToDb(message);
+  }
+
+}
