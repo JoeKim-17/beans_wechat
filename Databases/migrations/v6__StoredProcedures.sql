3ceb1c4ecf148eee8740296c0a8dea29b592ae76
hleketaniDuncan97 <duncanb@bbd.co.za>
2024-03-13 22:30:10 +0200

    Update the stored procs Scripts

--- Databases/migrations/v6__StoredProcedures.sql
+++ Databases/migrations/v6__StoredProcedures.sql
@@ -1,9 +1,11 @@
-DROP PROCEDURE InsertUser;
+DROP PROCEDURE InsertUser;\
+GO
 
 DROP PROCEDURE InsertIntoChat;
+GO
 
 DROP PROCEDURE InsertMessage;
-
+GO
 
 CREATE PROCEDURE InsertUser
     @UserName VARCHAR(100),
@@ -15,6 +17,7 @@ BEGIN
       VALUES (@UserName, @EmailAddress, @MobileNo)
 END;
 
+GO
 --Stored proc for chat table
 CREATE PROCEDURE InsertIntoChat
     @Sender VARCHAR(100), 
@@ -24,13 +27,14 @@ BEGIN
       INSERT INTO Chat (Sender, Receiver)
       VALUES ((SELECT UserId FROM Users WHERE UserName=@Sender),( SELECT UserId FROM Users WHERE UserName= @Receiver));
 END;
+GO
 
 CREATE PROCEDURE InsertMessage
    @ChatId INT,
    @Content VARCHAR(Max)
 AS
 BEGIN 
-      INSERT INTO Message(ChatId,Content)
-	  VALUES (@ChatId,@Content);
+     INSERT INTO Message(ChatId,Content)
+	 VALUES (@ChatId,@Content);
 END;
-
+GO
