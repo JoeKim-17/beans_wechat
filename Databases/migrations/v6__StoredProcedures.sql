c496c3af3a3303be9c37e6760fdb206552a17534
hleketaniDuncan97 <duncanb@bbd.co.za>
2024-03-13 22:13:39 +0200

    Update the Constrainst Scripts

---
+++ Databases/migrations/v6__StoredProcedures.sql
@@ -0,0 +1,36 @@
+DROP PROCEDURE InsertUser;
+
+DROP PROCEDURE InsertIntoChat;
+
+DROP PROCEDURE InsertMessage;
+
+
+CREATE PROCEDURE InsertUser
+    @UserName VARCHAR(100),
+    @EmailAddress NVARCHAR(500),
+    @MobileNo NVARCHAR(20)
+AS
+BEGIN
+      INSERT INTO Users (UserName, EmailAddress, MobileNo)
+      VALUES (@UserName, @EmailAddress, @MobileNo)
+END;
+
+--Stored proc for chat table
+CREATE PROCEDURE InsertIntoChat
+    @Sender VARCHAR(100), 
+    @Receiver VARCHAR(100)
+AS
+BEGIN
+      INSERT INTO Chat (Sender, Receiver)
+      VALUES ((SELECT UserId FROM Users WHERE UserName=@Sender),( SELECT UserId FROM Users WHERE UserName= @Receiver));
+END;
+
+CREATE PROCEDURE InsertMessage
+   @ChatId INT,
+   @Content VARCHAR(Max)
+AS
+BEGIN 
+      INSERT INTO Message(ChatId,Content)
+	  VALUES (@ChatId,@Content);
+END;
+
