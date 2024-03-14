8ea0076241a8f8d60eb1189dba3178dc30f7f9f6
Joseph Kimathi <Joseph.Kimathi@bbd.co.za>
2024-03-14 22:46:23 +0200

    use for github oatuh

---
+++ cli-wechat/levelup/src/main/java/com/levelup/model/Chat.java
@@ -0,0 +1,46 @@
+package com.levelup.model;
+
+public class Chat {
+
+  private int ChatId;
+  private String Sender;
+  private String Receiver;
+
+  public Chat(int ChatId, String Sender, String Receiver) {
+    this.ChatId = ChatId;
+    this.Sender = Sender;
+    this.Receiver = Receiver;
+  }
+
+  public Chat() {
+  }
+
+  public int getChatID() {
+    return ChatId;
+  }
+
+  public void setChatID(int ChatId) {
+    this.ChatId = ChatId;
+  }
+
+  public String getSender() {
+    return Sender;
+  }
+
+  public void setSender(String Sender) {
+    this.Sender = Sender;
+  }
+
+  public String getReceiver() {
+    return Receiver;
+  }
+
+  public void setReceiver(String Receiver) {
+    this.Receiver = Receiver;
+  }
+
+  @Override
+  public String toString() {
+    return ChatId + ": " + Sender + " & " + Receiver;
+  }
+}
